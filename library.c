25d817abd4365a61f53696d2d1bb08a0cd1bb0da
Benjamin Wright <allwrightyben@proton.me>
2026-02-20 20:11:43 +0800

    Removes hello world

--- library.c
+++ library.c
@@ -1,7 +1,44 @@
 #include "library.h"
 
-#include <stdio.h>
+#include <altcore/types.h>
+#include <altcore/malloc.h>
+#include <altcore/arenas.h>
+#include <altcore/strings.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+
+static Arena g_arena = {};
+static bool g_initialized = false;
+
+void article_init() {
+    if (!g_initialized) {
+        i64 malloc_cap = 1024LL * 1024LL;
+        alt_init(malloc_cap);
+
+        g_arena = arena_make(malloc_cap / 2);
+
+        g_initialized = true;
+    }
+}
+
+void article_uninit() {
+    if (g_initialized) {
+        arena_free(&g_arena);
+        alt_uninit();
+
+        g_initialized = false;
+    }
+}
+
+char *article_to_html(const char *filepath) {
+    if (!filepath) {
+        return nullptr;
+    }
+
+    Arena tmp = g_arena;
+
+    string html = str_make(&tmp, "");
+
+    char *html_malloc = nullptr;
+
+    return html_malloc;
 }
--- library.h
+++ library.h
@@ -1,6 +1,10 @@
 #ifndef ARTICLE_HTML_LIBRARY_H
 #define ARTICLE_HTML_LIBRARY_H
 
-void hello(void);
+void article_init();
+
+void article_uninit();
+
+char *article_to_html(const char *filepath);
 
 #endif // ARTICLE_HTML_LIBRARY_H
