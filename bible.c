356519c2549f108047770ea0cad644729423c4c3
Benjamin Wright <allwrightyben@proton.me>
2026-04-01 21:26:00 +0800

    Passes parameter to hyperscript func

--- bible.c
+++ bible.c
@@ -475,7 +475,7 @@ string bible_passage_to_hover_ref_html(Arena *arena, BiblePassage passage) {
 
         str_append(
             &out_html,
-            "<span class=\"bible-hover-ref\" _=\"on mouseenter call OnBibleRefHover()\">"
+            "<span class=\"bible-hover-ref\" _=\"on mouseenter call OnBibleRefHover(me)\">"
         );
         str_append(&out_html, "%s", ref_str.data);
         str_append(&out_html, "</span>");
